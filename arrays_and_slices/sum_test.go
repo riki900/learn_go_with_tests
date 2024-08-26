5ca5c260f3775b65d3b8c48021f7b41ae18b8658
Rick Klein <kleinr900@gmail.com>
2024-08-26 13:44:47 -0700

    SumAll initial - array/slice chapter

--- arrays_and_slices/sum_test.go
+++ arrays_and_slices/sum_test.go
@@ -1,15 +1,42 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("collection of 5 numbers", func(t *testing.T) {
 
-	got := Sum(numbers)
-	want := 15
+		numbers := []int{1, 2, 3, 4, 5}
 
-	if got != want {
-		t.Errorf("expected %d, got %d", want, got)
+		got := Sum(numbers)
+		want := 15
+
+		if got != want {
+			t.Errorf("expected %d, got %d", want, got)
+		}
+	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("expected %d, got %d", want, got)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("expected %v, got %v", want, got)
 	}
 }
