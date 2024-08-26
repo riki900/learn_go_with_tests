3ec5990df01b808dbfc3a2ce606cfc89f1d96f62
Rick Klein <kleinr900@gmail.com>
2024-08-25 18:14:09 -0700

    iteration initial commit

---
+++ iteration/repeat_test.go
@@ -0,0 +1,14 @@
+package iteration
+
+import "testing"
+
+func TestRepeat(t *testing.T) {
+
+	repeated := Repeat("a")
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q got %q", expected, repeated)
+	}
+
+}
