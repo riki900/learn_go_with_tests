d6fdbc9f4021560d2d6e17bce15068d7d3842a19
Rick Klein <kleinr900@gmail.com>
2024-08-27 12:40:40 -0400

    refactored area tests to use t.run - chapter 05

--- 05_structs_methods_and_interfaces/shapes_test.go
+++ 05_structs_methods_and_interfaces/shapes_test.go
@@ -16,18 +16,22 @@ func TestPerimeter(t *testing.T) {
 func TestArea(t *testing.T) {
 
 	areaTests := []struct {
+		name  string
 		shape Shape
 		want  float64
 	}{
-		{Rectangle{12.0, 6.0}, 72.0},
-		{Circle{10}, 314.1592653589793},
+		{name: "rectangle", shape: Rectangle{Width: 12.0, Height: 6.0}, want: 72.0},
+		{name: "circle", shape: Circle{Radius: 10.0}, want: 314.1592653589793},
+		{name: "triangle", shape: Triangle{Base: 12.0, Height: 6.0}, want: 36.0},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("expected %g, got %g", tt.want, got)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.want {
+				t.Errorf("%#v expected %g, got %g", tt.shape, tt.want, got)
+			}
+		})
 	}
 
 }
