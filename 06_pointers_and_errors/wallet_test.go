e7a9b21ee844016b4e2351d149c6fbe88cb987d8
Rick Klein <kleinr900@gmail.com>
2024-08-27 15:30:22 -0400

    initial commit - chapter 06

---
+++ 06_pointers_and_errors/wallet_test.go
@@ -0,0 +1,20 @@
+package wallet
+
+import (
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+
+	wallet := Wallet{}
+
+	wallet.Deposit(Bitcoin(10))
+
+	got := wallet.Balance()
+
+	want := Bitcoin(10)
+
+	if got != want {
+		t.Errorf("expected %s, got %s", want, got)
+	}
+}
