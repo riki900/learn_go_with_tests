267e643cb92127c00ee0d969af1059fe44e46afa
Rick Klein <kleinr900@gmail.com>
2024-08-27 12:09:09 -0400

    added Shape interface - chapter 05

--- 05_structs_methods_and_interfaces/shapes_test.go
+++ 05_structs_methods_and_interfaces/shapes_test.go
@@ -15,24 +15,26 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		got := rectangle.Area()
-		want := 72.0
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+
+		got := shape.Area()
 
 		if got != want {
 			t.Errorf("expected %2f, got %2f", want, got)
 		}
+
+	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		want := 72.0
+		checkArea(t, rectangle, want)
 	})
 
 	t.Run("circles", func(t *testing.T) {
 		circle := Circle{10}
-		got := circle.Area()
 		want := 314.1592653589793
-
-		if got != want {
-			t.Errorf("expected %g, got %g", want, got)
-
-		}
+		checkArea(t, circle, want)
 	})
 }
