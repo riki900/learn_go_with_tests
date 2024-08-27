b4cdcf664b49fcc764f5c7f5b95d34b3a8fd5fbf
Rick Klein <kleinr900@gmail.com>
2024-08-27 15:53:29 -0400

    added insufficent funds check - chapter 06

--- 06_pointers_and_errors/wallet_test.go
+++ 06_pointers_and_errors/wallet_test.go
@@ -6,15 +6,43 @@ import (
 
 func TestWallet(t *testing.T) {
 
-	wallet := Wallet{}
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
 
-	wallet.Deposit(Bitcoin(10))
+		got := wallet.Balance()
 
-	got := wallet.Balance()
+		if got != want {
+			t.Errorf("expected %s, got %s", want, got)
+		}
 
-	want := Bitcoin(10)
-
-	if got != want {
-		t.Errorf("expected %s, got %s", want, got)
 	}
+
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+
+		wallet.Deposit(Bitcoin(10))
+
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+
+		wallet.Withdraw(Bitcoin(10))
+
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("insufficent funds withdraw", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{balance: startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertBalance(t, wallet, startingBalance)
+
+		if err == nil {
+			t.Error("Expected an error. No error returned")
+		}
+
+	})
 }
