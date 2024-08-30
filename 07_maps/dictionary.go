ce07587ba489e0a8c707dd0c291e200cd98f1e71
Rick Klein <kleinr900@gmail.com>
2024-08-30 10:05:00 -0400

    Dictionary search completed - chapter 07

--- 07_maps/dictionary.go
+++ 07_maps/dictionary.go
@@ -1,7 +1,17 @@
 package main
 
+import "errors"
+
+var ErrNotFound = errors.New("cannot find word in dictionary")
+
 type Dictionary map[string]string
 
-func Search(dictionary Dictionary, word string) string {
-	return dictionary[word]
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
 }
--- 07_maps/dictionary_test.go
+++ 07_maps/dictionary_test.go
@@ -6,14 +6,33 @@ func TestSearch(t *testing.T) {
 
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := Search(dictionary, "test")
-	want := "this is just a test"
+	t.Run("word found", func(t *testing.T) {
 
-	AssertStrings(t, got, want)
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+
+		AssertStrings(t, got, want)
+
+	})
+
+	t.Run("word not found", func(t *testing.T) {
+
+		_, err := dictionary.Search("not found")
+
+		AssertErrors(t, err, ErrNotFound)
+
+	})
 
 }
 
 func AssertStrings(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("expected %q, got %q", want, got)
+	}
+}
+
+func AssertErrors(t testing.TB, got, want error) {
 	t.Helper()
 	if got != want {
 		t.Errorf("expected %q, got %q", want, got)
